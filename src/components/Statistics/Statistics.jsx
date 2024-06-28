ce4e04a62a1a898afd57a8d70151a953cf9e01c0
Anastasiia Tarasiuk <111896564+anastasiiatarasiuk@users.noreply.github.com>
2024-06-28 22:30:54 +0300

    commit to statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,12 +3,14 @@ import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 import { GiTreeDoor } from 'react-icons/gi';
 import { StatisticsItem } from '../StatisticsItem/StatisticsItem';
 import style from './Statistics.module.css';
+
 const icons = [
   <FaRegThumbsUp key="FaRegThumbsUp" />,
   <MdPeople key="MdPeople" />,
   <MdOutlineProductionQuantityLimits key="MdOutlineProductionQuantityLimits" />,
   <GiTreeDoor key="GiTreeDoor" />,
 ];
+
 export const Statistics = ({ title, stats }) => {
   return (
     <>
